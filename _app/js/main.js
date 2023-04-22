2d881b275019cb61fc5bac3a5920c45241160186
Anna Eikrem <eikrem.anna@gmail.com>
2023-04-22 14:05:39 +0200

    Edited file name

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,6 +1,6 @@
 import fetchProductsData from "./modules/fetch-products-data.js";
 
-import toggleNavigation from "./modules/toggleNavigation.js";
+import toggleNavigation from "./modules/toggle-navigation.js";
 import renderProductCards from "./modules/render-products.js";
 
 
