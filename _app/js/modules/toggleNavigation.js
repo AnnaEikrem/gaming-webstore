b203fb48901f94d2621ad13fcd6981f448b9b92c
Anna Røsøvåg Eikrem <112336610+AnnaEikrem@users.noreply.github.com>
2023-04-22 12:13:11 +0200

    Feature: toggle navigation (#6)
    * Changed html order of buttons
    
    * Added hover effect for > 768px
    
    * Edited media query
    
    * Changed styling for header
    
    * Added toggleNavigation functionality

--- _app/js/modules/toggleNavigation.js
+++ _app/js/modules/toggleNavigation.js
@@ -1,3 +1,26 @@
 export default function toggleNavigation() {
-	console.log('hei');
+	const navigationOpenButton = document.querySelector('.navigation__open--button');
+	const navigationCloseButton = document.querySelector('.navigation__close--button');
+	const navigationContainer = document.querySelector('.header__navigation');
+	const body = document.querySelector('body');
+
+	navigationOpenButton.addEventListener('click', handleOpenButtonClick);
+
+	navigationCloseButton.addEventListener('click', handleCloseButtonClick);
+	
+
+	function handleOpenButtonClick() {
+		navigationContainer.classList.add('header__navigation--visible');
+		navigationOpenButton.style.display = 'none';
+		navigationCloseButton.style.display = 'block';
+		body.classList.add('no-scroll');
+	}
+
+
+	function handleCloseButtonClick() {
+		navigationContainer.classList.remove('header__navigation--visible');
+		navigationOpenButton.style.display = 'block';
+		navigationCloseButton.style.display = 'none';
+		body.classList.remove('no-scroll');
+	}
 }
