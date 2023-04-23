273d11e6af0a32aa2607f6fe78a31eb194c26519
Anna Røsøvåg Eikrem <112336610+AnnaEikrem@users.noreply.github.com>
2023-04-23 16:37:14 +0200

    Feature: Rendering product cards (#9)
    * Changed class name
    
    * Rendered products for 'chosen' and 'all products'.

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,10 +1,8 @@
 import fetchProductsData from "./modules/fetch-products-data.js";
-
 import toggleNavigation from "./modules/toggle-navigation.js";
 import renderProductCards from "./modules/render-products.js";
 
 const fetchedData = await fetchProductsData();
 
 renderProductCards(fetchedData);
-
 toggleNavigation();
