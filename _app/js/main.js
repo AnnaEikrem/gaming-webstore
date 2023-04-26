66f44fe4d02fde4195d68baac50cc39a3de76aa9
Anna Røsøvåg Eikrem <112336610+AnnaEikrem@users.noreply.github.com>
2023-04-26 19:04:27 +0200

    Feature: product detailes site (#12)
    * Added folder and html for details page
    
    * Created and linked css file for product-details
    
    * Created js file for 'Product-review'
    
    * Misc changes to header styling
    
    * Changed class name
    
    * Changes to variables and applied to card-style
    
    * Changed file name
    
    * Removed comments and edited query
    
    * Commented out HTML Mockup
    
    * Started creating export Product-preview function

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,8 +1,10 @@
 import fetchProductsData from "./modules/fetch-products-data.js";
 import toggleNavigation from "./modules/toggle-navigation.js";
 import renderProductCards from "./modules/render-products.js";
+import previewProduct from "./modules/product-preview.js";
 
 const fetchedData = await fetchProductsData();
 
 renderProductCards(fetchedData);
 toggleNavigation();
+previewProduct(fetchedData);
---
+++ _app/js/modules/product-preview.js
@@ -0,0 +1,121 @@
+export default function previewProduct(products) {
+	const productContainer = document.querySelector('.product__preview--information');
+	const currentSearchParams = new URLSearchParams(window.location.search);
+	const productSlug = currentSearchParams.get('id');
+
+	if (productContainer) {
+		renderPreviewHTML(productSlug);
+	}
+
+
+	function returnDOMElement(product) {
+		const previewCardElement = document.createElement('div');
+		previewCardElement.classList.add('product__preview--card');
+
+		const cardTopContainer = document.createElement('div');
+		cardTopContainer.classList.add('product__card--top');
+
+		const cardTopHeadline = document.createElement('h3');
+		cardTopHeadline.classList.add('product__detail--name');
+		cardTopHeadline.textContent = product.name;
+
+		const cardtopImage = document.createElement('div');
+		cardtopImage.classList.add('product__detail--image');
+		const imageElementSource = document.createElement('img');
+		imageElementSource.classList.add('product__detail--image--source');
+
+		const cardBottomContainer = document.createElement('div');
+		cardBottomContainer.classList.add('product__card--bottom');
+
+		const cardBottomContent = document.createElement('div');
+		cardBottomContent.classList.add('product__details--content');
+
+		// Three divs with three contents. Need another solution.
+		const cardBottomDetail = document.createElement('div');
+		cardBottomDetail.classList.add('product__detail');
+
+		const cardBottomDetailText = document.createElement('div');
+		cardBottomDetailText.classList.add('product__detail--text');
+		cardBottomDetailText.textContent = 'Price';
+
+		const cardBottomDetailValue = document.createElement('div');
+		cardBottomDetailValue.classList.add('product__detail--value');
+
+
+		// Buttons
+		const cardButtonsContainer = document.createElement('div');
+		cardButtonsContainer.classList.add('product__card--buttons');
+
+		const buttonQuantity = document.createElement('div');
+		buttonQuantity.classList.add('card__button--quantity');
+		const buttonSubtract = document.createElement('button');
+		buttonSubtract.classList.add('button__quantity--subtract');
+		buttonSubtract.textContent = '-'
+
+		const buttonNumber = document.createElement('div');
+		buttonNumber.classList.add('button__quantity--number');
+		// buttonNumber.textContent = product.
+
+		const buttonAdd = document.createElement('button');
+		buttonAdd.classList.add('button__quantity--add');
+		buttonAdd.textContent = '+'
+
+		const buttonAddToCart = document.createElement('button');
+		buttonAddToCart.classList.add('card__button--add-to-cart');
+		buttonAddToCart.textContent = 'Add to cart';
+
+		const cardDescriptionContainer = document.createElement('div');
+		cardDescriptionContainer.classList.add('product__card--description');
+
+		const descriptionHeadline = document.createElement('h4');
+		descriptionHeadline.classList.add('card__description--headline');
+		descriptionHeadline.textContent = 'Description';
+
+		const descriptionText = document.createElement('article');
+		descriptionText.classList.add('card__description--text');
+		descriptionText.textContent = product.description;
+
+		console.log(product.slug)
+
+		// AppendChild to create HTML structure
+		previewCardElement.appendChild(cardTopContainer);
+		previewCardElement.appendChild(cardBottomContainer);
+		previewCardElement.appendChild(cardButtonsContainer);
+		previewCardElement.appendChild(cardDescriptionContainer);
+
+		cardTopContainer.appendChild(cardTopHeadline);
+		cardTopContainer.appendChild(cardtopImage);
+		cardtopImage.appendChild(imageElementSource);
+
+		cardBottomContainer.appendChild(cardBottomContent);
+		cardBottomContent.appendChild(cardBottomDetail);
+
+		cardBottomDetail.appendChild(cardBottomDetailText);
+		cardBottomDetail.appendChild(cardBottomDetailValue);
+		// What to do about color?
+
+		cardButtonsContainer.appendChild(buttonQuantity);
+		cardBottomContainer.appendChild(buttonAddToCart);
+
+		buttonQuantity.appendChild(buttonSubtract);
+		buttonQuantity.appendChild(buttonNumber);
+		buttonQuantity.appendChild(buttonAdd);
+
+		cardDescriptionContainer.appendChild(descriptionHeadline);
+		cardDescriptionContainer.appendChild(descriptionText);
+
+		return previewCardElement
+	}
+
+
+	function renderPreviewHTML(slug) {
+		const currentProduct = products.find(product => product.slug === slug);
+		const currentProductClicked = returnDOMElement(currentProduct);
+
+		productContainer.appendChild(currentProductClicked)
+	};
+
+
+
+	// console.log(products);
+}
