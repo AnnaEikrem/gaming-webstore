af66959d5bf68e8a6c3d87bbe89095c389651a79
Anna Eikrem <eikrem.anna@gmail.com>
2023-04-28 14:47:32 +0200

    Created 'slides' for product-images with js

--- _app/js/modules/imageSlides.js
+++ _app/js/modules/imageSlides.js
@@ -1,3 +1,40 @@
 export default function imageSlides() {
-	console.log('hei')
+	let currentSlideIndex = 0;
+	const slidesContainer = document.querySelector('.product__images--slides')
+	const slides = document.querySelectorAll('.product__image--slide');
+	const previousButton = document.querySelector('.image__button--previous');
+	const nextButton = document.querySelector('.image__button--next');
+
+	if (slidesContainer) {
+		displaySlide(currentSlideIndex);
+		previousButton.addEventListener('click', handlePreviousButtonClick);
+		nextButton.addEventListener('click', handleNextButtonClick);
+	};
+
+	function displaySlide(index) {
+		for (let i = 0; i < slides.length; i++) {
+			slides[i].style.display = 'none';
+		}
+
+		slides[index].style.display = 'block';
+	}
+
+
+	function handlePreviousButtonClick() {
+		currentSlideIndex--;
+		if (currentSlideIndex < 0) {
+			currentSlideIndex = slides.length - 1;
+		}
+
+		displaySlide(currentSlideIndex);
+	};
+
+	function handleNextButtonClick() {
+		currentSlideIndex++;
+		if (currentSlideIndex >= slides.length) {
+			currentSlideIndex = 0;
+		}
+
+		displaySlide(currentSlideIndex);
+	};
 }
