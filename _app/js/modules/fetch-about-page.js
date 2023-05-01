51f2aac222db3d2615a78f12b283c856a3ab1c67
Anna Eikrem <eikrem.anna@gmail.com>
2023-05-01 15:42:45 +0200

    Added GROQ query to fetch 'About' page data

--- _app/js/modules/fetch-about-page.js
+++ _app/js/modules/fetch-about-page.js
@@ -1,5 +1,14 @@
 import { sanity } from '../sanity.js';
 
 export default async function aboutPage() {
-	console.log('anna')
+	const pageQuery = `*[_type == 'pages'] {
+		name,
+		headline,
+		description,
+	}`;
+
+	const params = {};
+	const pageData = await sanity.fetch(pageQuery, params);
+
+	return pageData;
 }
