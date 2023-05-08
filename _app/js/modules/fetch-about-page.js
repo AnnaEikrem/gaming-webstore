b1b98d50333aad4a2aaea335ee69ec65e73b2e3f
Anna Røsøvåg Eikrem <112336610+AnnaEikrem@users.noreply.github.com>
2023-05-08 11:34:16 +0200

    Created try/catch for fetching, but in comments (#26)

--- _app/js/modules/fetch-about-page.js
+++ _app/js/modules/fetch-about-page.js
@@ -1,14 +1,41 @@
 import { sanity } from '../sanity.js';
 
 export default async function aboutPage() {
-	const pageQuery = `*[_type == 'pages'] {
-		name,
-		headline,
-		description,
-	}`;
+		const pageQuery = `*[_type == 'pages'] {
+			name,
+			headline,
+			description,
+		}`;
 
-	const params = {};
-	const pageData = await sanity.fetch(pageQuery, params);
+		const params = {};
+		const pageData = await sanity.fetch(pageQuery, params);
 
-	return pageData;
+		return pageData;
 }
+
+// export default async function aboutPage() {
+// 	try {
+// 		const pageQuery = `*[_type == 'pages'] {
+// 			name,
+// 			headline,
+// 			description,
+// 		}`;
+
+// 		const params = {};
+// 		const pageData = await sanity.fetch(pageQuery, params);
+
+// 		return pageData;
+// 	} catch (error) {
+// 		console.error(error);
+
+// 		if (error.response && error.response.status === 404) {
+// 			throw new Error('Url not existing');
+// 		} else if (error.response && error.response.status === 401) {
+// 			throw new Error('Not authorized user');
+// 		} else if (error.response && error.response.status >= 500) {
+// 			throw new Error('Server not responding');
+// 		} else {
+// 			throw new Error('Something went wrong');
+// 		}
+// 	}
+// }
