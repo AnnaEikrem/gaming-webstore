a423b7abc832235861df035e5cc8bf9d1949dba6
Anna Røsøvåg Eikrem <112336610+AnnaEikrem@users.noreply.github.com>
2023-05-21 11:53:49 +0200

    Feature: shopping cart (#32)
    * Commented out console.log
    
    * Removed html
    
    * Added focus styling to search-filter list
    
    * Changed searchFilter function
    
    * Started shopping cart functionality

--- _app/js/modules/shopping-cart.js
+++ _app/js/modules/shopping-cart.js
@@ -22,58 +22,87 @@ export default function shoppingCart(products) {
 
 	function addItemToCart(productData) {
 		const cardData = productData.dataset;
-		const product = {
+		const productObject = {
 			id: cardData.id,
 			name: cardData.name,
-			price: cardData.price,
+			price: parseInt(cardData.price),
 			quantity: 1,
 		};
-		
-		const matchInCart = cartProductsList.find(product => {
-			product.id === cardData.id
-		});
+
+		const matchInCart = cartProductsList.find(product => product.id === cardData.id
+		);
 
 		if (matchInCart) {
 			matchInCart.quantity += 1;
-		} else {
-			cartProductsList.push(product);
-		}
-		console.log(cardData);
-	}
-
-	function renderCart() {
-		// if (cartProductsList.length > 0) {
-
+		} 
+		// else {
+		// 	cartListContainer.textContent = '';
+		// 	cartTotalSum.textContent = '0.00';
 		// }
 
 		insertCartItem();
+
 	}
 
-	// function insertCartItem() {
-	// 	const cartItem = document.createElement('div');
-	// 	const cartItemImage = document.createElement('img');
-	// 	const cartItemInformation = document.createElement('div');
-	// 	const itemInformationTitle = document.createElement('div');
-	// 	const itemInformationPrice = document.createElement('div');
-	// 	const itemRemoveButton = document.createElement('button');
+	function renderCart() {
+		if (cartProductsList.length > 0) {
+			const cartTotalItems = cartProductsList.reduce((total, currentProduct) => {
+				return total + currentProduct.quantity;
+			}, 0);
 
-	// 	cartItem.classList.add('cart__product--item');
-	// 	cartItemImage.classList.add('cart__product--image');
-	// 	cartItemInformation.classList.add('cart__product--information');
-	// 	itemInformationTitle.classList.add('cart__product--title');
-	// 	itemInformationPrice.classList.add('cart__product--price');
-	// 	itemRemoveButton.classList.add('cart__product--remove--button');
+			const cartTotalSumValue = cartProductsList.reduce((total, currentProduct) => {
+				return total + (currentProduct.price * currentProduct.quantity);
+			}, 0)
 
-	// 	cartItemImage.setAttribute('src', `/_app/assets/Monkey.gif`)
-	// 	itemInformationTitle.textContent = `Game Boy`
-	// 	itemInformationPrice.textContent = `1500kr`
-	// 	itemRemoveButton.textContent = `Remove`;
+			cartTotalSum.textContent = cartTotalSumValue.toFixed(2);
+		} 
+		else {
+			cartListContainer.textContent = '';
+			cartTotalSum.textContent = '0.00';
+		}
+		
+		insertCartItem();
+	}
 
-	// 	cartListContainer.appendChild(cartItem);
-	// 	cartItem.appendChild(cartItemImage);
-	// 	cartItem.appendChild(cartItemInformation);
-	// 	cartItemInformation.appendChild(itemInformationTitle);
-	// 	cartItemInformation.appendChild(itemInformationPrice);
-	// 	cartItemInformation.appendChild(itemRemoveButton);
-	// }
+	function insertCartItem() {
+		// cartListContainer.textContent = '';
+
+		cartProductsList.forEach(product => {
+
+		const cartItem = document.createElement('div');
+		// const cartItemImage = document.createElement('img');
+		const cartItemInformation = document.createElement('div');
+		const itemInformationTitle = document.createElement('div');
+		const itemInformationPrice = document.createElement('div');
+		const itemRemoveButton = document.createElement('button');
+
+		cartItem.classList.add('cart__product--item');
+		// cartItemImage.classList.add('cart__product--image');
+		cartItemInformation.classList.add('cart__product--information');
+		itemInformationTitle.classList.add('cart__product--title');
+		itemInformationPrice.classList.add('cart__product--price');
+		itemRemoveButton.classList.add('cart__product--remove--button');
+
+		// cartItemImage.setAttribute('src', `/_app/assets/Monkey.gif`)
+		console.log(product)
+		// itemInformationTitle.textContent = product.name;
+		// itemInformationPrice.textContent = `${product.price} kr x ${product.quanity}`;
+		itemRemoveButton.textContent = `Remove`;
+
+		cartListContainer.appendChild(cartItem);
+		// cartItem.appendChild(cartItemImage);
+		cartItem.appendChild(cartItemInformation);
+		cartItemInformation.appendChild(itemInformationTitle);
+		cartItemInformation.appendChild(itemInformationPrice);
+		cartItemInformation.appendChild(itemRemoveButton);
+
+			itemRemoveButton.addEventListener('click', handleItemRemoveButton)
+
+			function handleItemRemoveButton() {
+				const index = cartProductsList.indexOf(product);
+				cartProductsList.splice(index, 1);
+				renderCart();
+			}
+		})
+	}
 }
