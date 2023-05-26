bb4958cabe5e4f1986043cf00ff8103a818e1fc8
Anna Eikrem <eikrem.anna@gmail.com>
2023-05-26 12:27:22 +0200

    Edited header vh in mobile version

--- _app/js/modules/toggle-navigation.js
+++ _app/js/modules/toggle-navigation.js
@@ -1,11 +1,15 @@
+/**
+ * @todo Design header toggling in mobile, to better accomodate the 'filter-search' function, when rendering the results
+ */
+
 export default function toggleNavigation() {
+	// Variables
 	const navigationOpenButton = document.querySelector('.navigation__open--button');
 	const navigationCloseButton = document.querySelector('.navigation__close--button');
 	const navigationContainer = document.querySelector('.header__navigation');
-	const body = document.querySelector('body');
 
+	// EventListeners
 	navigationOpenButton.addEventListener('click', handleOpenButtonClick);
-
 	navigationCloseButton.addEventListener('click', handleCloseButtonClick);
 	
 
@@ -13,14 +17,11 @@ export default function toggleNavigation() {
 		navigationContainer.classList.add('header__navigation--visible');
 		navigationOpenButton.style.display = 'none';
 		navigationCloseButton.style.display = 'block';
-		body.classList.add('no-scroll');
 	}
-
 
 	function handleCloseButtonClick() {
 		navigationContainer.classList.remove('header__navigation--visible');
 		navigationOpenButton.style.display = 'block';
 		navigationCloseButton.style.display = 'none';
-		body.classList.remove('no-scroll');
 	}
 }
